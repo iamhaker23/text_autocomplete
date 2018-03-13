2b8cd607cf55b486dc36fb9f2ec95856166b9084
Hakeem Bux <h.bux@uea.ac.uk>
2018-03-13 21:22:53 +0000

    Added sorting of autocomplete matches, added input parsing

--- src/dsacoursework2/AutoCompletion.java
+++ src/dsacoursework2/AutoCompletion.java
@@ -3,7 +3,11 @@ package dsacoursework2;
 import static dsacoursework2.DictionaryFinder.getLocalResourcesPath;
 import static dsacoursework2.DictionaryFinder.readWordsFromCSV;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  *
@@ -19,9 +23,11 @@ public class AutoCompletion {
         for (String key : allMatches.keySet()){
             
             Float current = allMatches.get(key);
-                    
+            
             for (int i = 0; i < best.length; i++){
-                if (current != null && ((best[i] == null) || (allMatches.get(best[i]) < current))){
+                
+                if (current != null && ((best[i] == null) || (current.compareTo(allMatches.get(best[i]))) > 0) && (Arrays.asList(best).indexOf(key) == -1)){
+                    
                     best[i] = key;
                     i = best.length;
                 }
@@ -65,38 +71,94 @@ public class AutoCompletion {
         
         for (String word : df.dictionary.keySet()){
             
-            T.add(word, df.dictionary.get(word), false);
+            if (word != null && !word.equals("")) T.add(word, df.dictionary.get(word), false);
             
         }
         
-        ArrayList<String> queries =  readWordsFromCSV(queryFileName, (char)13);
+        ArrayList<String> queries =  readWordsFromCSV(queryFileName, '\n');
         
         HashMap<String, HashMap<String, Float>> autoCompleteMatches = new HashMap<>();
         
         for (String query : queries){
             
-            /*for (String val : T.getSubTrie(query).getAllWords()){
-                System.out.println(val);
-            }
-            System.out.println("\n" + query + "\n");*/
-            
-            HashMap<String, Float> matches = T.getBestMatches(query);
-            
-            if (matches != null){
+            if (query != null && !query.equals("")){
                 
-                autoCompleteMatches.put(query, matches);
+                query = query.trim();
 
-                printBestThreeMatches(matches, query);
-                
+                /*for (String val : T.getSubTrie(query).getAllWords()){
+                    System.out.println(val);
+                }
+                System.out.println("\n" + query + "\n");*/
+
+                HashMap<String, Float> matches = T.getBestMatches(query);
+
+                LinkedHashMap<String, Float> sortedMatches = sortMatches(matches);
+
+                if (sortedMatches != null){
+
+                    autoCompleteMatches.put(query, sortedMatches);
+                    System.out.println("Given:" + query);
+                    String[] sortedKeys = sortedMatches.keySet().toArray(new String[0]);
+                    for (int i = 0; i < 3; i++){
+                        if (sortedKeys.length > i) printMatch(sortedKeys[i], sortedMatches.get(sortedKeys[i]), query);
+                    }
+
+                }
+                else System.out.println("Could not autocomplete word: '"+query+"'");
+            
+            //TODO: perhaps System.err.println()
+            } else System.out.println("Skipping invalid query value: '"+query+"'");
+        }
+
+            DictionaryFinder.saveMapToFile(autoCompleteMatches, getLocalResourcesPath("lotrMatches.csv"));
+
+            //df.saveToFile();
+    }
+    
+    private static void printMatch(String match, Float value, String query){
+        System.out.println("\t" + ((query.equals(match))?"" : query) + match + "(" + value + ")");
+    }
+    
+    //helper function to sort matches in order of highest probability (and alphabetically in the case of a tie)
+    private static LinkedHashMap<String, Float> sortMatches(HashMap<String, Float> matches){
+        
+        LinkedHashMap<Float, ArrayList<String>> probsToStrings = new LinkedHashMap<>();
+        
+        LinkedHashMap<String, Float> sortedMatches = new LinkedHashMap<>();
+        
+        //TODO: sort
+        
+        for (String key : matches.keySet()){
+            
+            Float probability = matches.get(key);
+            
+            if (probability !=null){
+                if (probsToStrings.get(probability) != null){
+                    probsToStrings.get(probability).add(key);
+                }else probsToStrings.put(probability, new ArrayList<>(Arrays.asList(new String[]{key})));
             }
-            else System.out.println("Could not autocomplete word: '"+query+"'");
             
         }
         
-        DictionaryFinder.saveMapToFile(autoCompleteMatches, getLocalResourcesPath("lotrMatches.csv"));
+        ArrayList<Float> sortedKeys = new ArrayList<>(Arrays.asList(probsToStrings.keySet().toArray(new Float[0])));
         
-        //df.saveToFile();
+        //TODO: refactor this to use comparators or something nicer
+        //Sort in descending order
+        Collections.sort(sortedKeys);
+        Collections.reverse(sortedKeys);
         
+        for (Float key : sortedKeys){
+            
+            //alphabetically sort the matches for this probability
+            List<String> words = probsToStrings.get(key);
+            Collections.sort(words);
+            
+            for (String word : words){
+                sortedMatches.put(word, key);
+            }
+        }
+        
+        return sortedMatches;
     }
     
     
